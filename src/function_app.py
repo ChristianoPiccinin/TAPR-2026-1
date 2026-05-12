777a3c4d8b970f0a71f2b9342dddf99b22ea7161
Christiano Piccinin <christiano.piccinin@gmail.com>
2026-05-11 22:19:23 -0300

    adicionar conexão com azure database

--- src/function_app.py
+++ src/function_app.py
@@ -8,5 +8,5 @@ from triggers.extract_csat import app as extract_csat
 from triggers.extract_sla import app as extract_sla
 
 app.register_functions(chamado)
-app.register_functions(extract_csat)
-app.register_functions(extract_sla)
+# app.register_functions(extract_csat)
+# app.register_functions(extract_sla)
--- src/triggers/extract_chamado.py
+++ src/triggers/extract_chamado.py
@@ -1,9 +1,63 @@
 import logging
 import azure.functions as func
+import os
+import pyodbc
+import json
+
 
 app = func.Blueprint()
 
-@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
+@app.timer_trigger(schedule="0 */2 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def extract_chamado(myTimer: func.TimerRequest) -> None:
-    logging.info('tabela chamado')
+
+    logging.info("Iniciando leitura da tabela itsm.chamado")
+ 
+    server = os.getenv("SQL_SERVER")
+    database = os.getenv("SQL_DATABASE")
+    username = os.getenv("SQL_USER")
+    password = os.getenv("SQL_PASSWORD")
+
+
+    logging.info(f"Server: {server}, Database: {database}, User: {username}, Password: {password}")
+
+
+    conn_str = (
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER={server};"
+        f"DATABASE={database};"
+        f"UID={username};"
+        f"PWD={password};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+
+    query = """
+        SELECT TOP (100) *
+        FROM itsm.chamado
+    """
+
+    try:
+        with pyodbc.connect(conn_str) as conn:
+            cursor = conn.cursor()
+            cursor.execute(query)
+
+            columns = [column[0] for column in cursor.description]
+            rows = cursor.fetchall()
+
+            data = [
+                dict(zip(columns, row))
+                for row in rows
+            ]
+
+            # logging.info(
+            #     json.dumps(data, default=str, ensure_ascii=False)
+            # )
+
+            logging.info(f"rows: {data[:5]}")  # Loga apenas os primeiros 5 registros para evitar excesso de log
+            #logging.info(f"Total de registros lidos: {len(data)}")
+
+    except Exception as e:
+        logging.error(f"Erro ao ler itsm.chamado: {str(e)}")
+        raise
